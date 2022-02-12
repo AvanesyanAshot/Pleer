562dae6b244b546454d3c1e7dce06204a11df2d2
AvanesyanAshot <9teron9@gmail.com>
2022-02-12 13:11:32 +0300

    feat: add prettier, eslint and lint-staged

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,11 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 const PORT = 8000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
+var a = 0;
 app.listen(PORT, () => {
   console.log(`[server]: Server is running at https://localhost:${PORT}`);
 });

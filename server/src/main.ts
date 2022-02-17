c4133fe921155137e8fad47fd03973e9013ba849
AvanesyanAshot <9teron9@gmail.com>
2022-02-17 10:23:23 +0300

    feat: add middleware ValidationPipe

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
@@ -6,8 +7,10 @@ const start = async () => {
   try {
     const PORT = process.env.PORT || 5000;
     const app = await NestFactory.create(AppModule);
-    app.enableCors();
+    app.useGlobalPipes(new ValidationPipe());
     app.use(cookieParser());
+
+    app.enableCors();
     await app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
   } catch (e) {
     console.log(e);
